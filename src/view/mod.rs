cdaa49043e43e3e2c9621ecbabaf588076ade687
Fee <Frederik.Peukert@gmx.de>
2026-05-23 02:01:07 +0200

    start new

--- src/view/mod.rs
+++
@@ -1,6 +0,0 @@
-//! Element builders from layout model.
-
-pub mod proportional;
-pub mod tree;
-
-pub use tree::build_layout;
--- src/widget/mod.rs
+++
@@ -1,5 +0,0 @@
-//! Custom iced widgets for docking UI.
-
-pub mod dock_surface;
-pub mod drop_overlay;
-pub mod tab_dock;
